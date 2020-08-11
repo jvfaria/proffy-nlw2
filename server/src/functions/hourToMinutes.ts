220582fd488f6ce2126202bd995f11ba3a1d0913
JVitor22 <jvfaria015@gmail.com>
2020-08-11 19:45:55 -0300

    util functions

---
+++ server/src/functions/hourToMinutes.ts
@@ -0,0 +1,10 @@
+export default function convert(time: string) {
+  const [hour, minutes]  = time.split(':').map(Number);
+
+  const timeInMinutes = (hour * 60) + minutes;
+
+  return timeInMinutes;
+ 
+
+
+}
